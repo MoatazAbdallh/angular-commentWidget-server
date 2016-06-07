76a792420e630f64cc72ae19096e8968790e7799
Moataz Abdallh <moataz.abdallh@gmail.com>
2016-06-07 23:47:32 +0200

    Adding createApp and postComment Services

---
+++ server.js
@@ -0,0 +1,94 @@
+// =======================
+// get the packages we need ============
+// =======================
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var base64 = require('base-64');
+var config = require('./config');
+
+var commentService = require('./services/comment-app');
+
+//depending on Config Vars in Heroku for production only else depend on config.js
+if (process.env.NODE_ENV == "production")
+    mongoose.connect(process.env.MONGODB_URI);
+else
+    mongoose.connect(config.database);
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', function (callback) {
+    console.log("Connection Success");
+});
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+
+app.use(allowCrossDomain);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+app.get('/', function (req, res) {
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+app.post('/create-comment-app', function (req, res) {
+    commentService.createApp(req.body).then(function (result) {
+        res.json({
+            errorCode: 0,
+            errorMessage: "",
+            data: {
+                comment: result
+            }
+        });
+    }, function (err) {
+        res.json({
+            errorCode: 1,
+            errorMessage: err.message,
+            data: null
+        });
+    });
+});
+app.post('/post-comment', function (req, res) {
+    commentService.postComment(req.body).then(function (result) {
+        res.json({
+            errorCode: 0,
+            errorMessage: "",
+            data: {
+                comment: result
+            }
+        });
+    }, function (err) {
+        res.json({
+            errorCode: 1,
+            errorMessage: err.message,
+            data: null
+        });
+    });
+});
+
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
---
+++ services/comment-app.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+    var $q = require('q');
+    var Comment = require('../models/comment');
+    var App = require('../models/app');
+    var random = require("random-js")();
+
+    var createApp = function (data) {
+        var defered = $q.defer();
+        var newApp = App({
+            app_id: random.string(5),
+            admin_email: data.admin_email,
+            comment_ids: [],
+            comments_count: 0,
+            recommendation_count: 0,
+            creation_date: new Date()
+        });
+        newApp.save(function (err, model) {
+            if (err)
+                defered.reject(err);
+            else
+            defered.resolve(model._doc)
+        });
+        return defered.promise;
+
+    };
+
+    var createComment = function (data) {
+        var defered = $q.defer();
+
+        var newComment = Comment({
+            user_name: data.user_name,
+            comment_txt: data.comment_txt,
+            points_up: 0,
+            points_down: 0,
+            creation_date: new Date()
+        });
+        newComment.save(function (err, comment) {
+            if (err)
+                defered.reject(err);
+            else {
+                var selectedApp = App.update(
+                    {app_id: data.app_id},
+                    {$push: {"comment_ids": comment._id.toString()}},
+                    function (err, model) {
+                        if (err)
+                            defered.reject(err);
+                        else
+                            defered.resolve(comment);
+                    }
+                );
+            }
+        });
+        return defered.promise;
+    };
+
+    module.exports = {
+        createApp: createApp,
+        postComment: createComment
+    }
+})();

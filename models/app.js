d7c6beb8a73bbddb449acac2b0565c57b5966930
Moataz Abdallh <moataz.abdallh@gmail.com>
2016-06-07 23:46:48 +0200

    Adding Models Schema

---
+++ models/app.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+    var mongoose = require('mongoose');
+
+    var AppSchema = new mongoose.Schema({
+            app_id:String,
+            admin_email: String,
+            admin_password: String,
+            comment_ids: [],
+            comments_count: Number,
+            recommendation_count: Number,
+            creation_date: Date
+        },
+        {collection: "App"}
+    );
+
+    module.exports = mongoose.model('User', AppSchema);
+})();
+
+
+
---
+++ models/comment.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+    var mongoose = require('mongoose');
+
+    var CommentSchema = new mongoose.Schema({
+            user_name: String,
+            comment_txt: String,
+            replies: [],
+            points_up: Number,
+            points_down: Number,
+            creation_date: Date
+        },
+        {collection: "Comment"}
+    );
+
+    module.exports = mongoose.model('Comment', CommentSchema);
+})();
+
+
+
